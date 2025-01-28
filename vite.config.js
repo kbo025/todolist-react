170bd2a0b251781dca8101d1e205488100d57b77
kbo025 <kbo025@gmail.com>
2025-01-28 20:14:30 -0300

    conf: deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "http://kbo025.github.io/todolist-react",
+  server: {
+    https: true,
+  },
+  build: {
+    outDir: 'dist',
+  },
   plugins: [
     tailwindcss(),
     react(),
